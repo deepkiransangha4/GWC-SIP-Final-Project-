3a45327768e0153f427c831b607d15af7fedc55e
AshZhang <ashley.w.zhang@gmail.com>
2016-08-03 15:02:48 -0700

    name saved between html files

--- enter_name_script.js
+++ enter_name_script.js
@@ -1,6 +1,7 @@
 function saveName(){
 	var elem = document.getElementById("name");
-	var name = elem.value;
-	console.log(name);
-	document.getElementById("printName").innerHTML = "Your name is "+name;
+	var yourname = elem.value;
+	window.name = elem.value;
+	console.log(yourname);
+	console.log(window.name);
 }
