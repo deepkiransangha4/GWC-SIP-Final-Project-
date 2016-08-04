4090433fdf43fabee4476046a4fe5eef4bf165c9
DiptiGupte <gupte.d50@gmail.com>
2016-08-04 09:39:14 -0700

    choose avatar

---
+++ choose_avatar.js
@@ -0,0 +1,23 @@
+function avatar1(){
+	var elem = document.getElementById("avatar1");
+	var your_avatar1 = elem.value;
+	window.name = elem.value;
+	console.log(your_avatar1);
+	console.log(window.name);
+}
+
+function avatar2(){
+	var elem = document.getElementById("avatar2");
+	var your_avatar2 = elem.value;
+	window.name = elem.value;
+	console.log(your_avatar2);
+	console.log(window.name);
+}
+
+function avatar3(){
+	var elem = document.getElementById("avatar3");
+	var your_avatar3 = elem.value;
+	window.name = elem.value;
+	console.log(your_avatar3);
+	console.log(window.name);
+}
