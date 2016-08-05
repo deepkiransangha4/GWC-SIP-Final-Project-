2595df80443058867cc526611a98df9f3445c60e
epickermann <espickermann2018@gmail.com>
2016-08-05 09:31:37 -0700

    rds

--- enter_name_script.js
+++ enter_name_script.js
@@ -7,3 +7,13 @@ function saveName(){
 	
 	console.log(sessionStorage.yourname);
 } 
+
+var friendship_points = 0
+
+function add_5_points(){
+		friendship_points +=5
+}
+
+function add_10_points(){
+		friendship_points +=10
+}
