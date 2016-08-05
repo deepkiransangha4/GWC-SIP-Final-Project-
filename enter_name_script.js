5f988f7d8867e170c612c9250859873c5aa7a106
epickermann <espickermann2018@gmail.com>
2016-08-05 10:29:42 -0700

    efsc

--- enter_name_script.js
+++ enter_name_script.js
@@ -10,10 +10,19 @@ function saveName(){
 
 var friendship_points = 0
 
-function add_5_points(){
-		friendship_points +=5
+function add_3_shells(){
+		friendship_points +=3
 }
 
-function add_10_points(){
-		friendship_points +=10
+function add_2_shells(){
+		friendship_points +=2
 }
+
+function add_1_shells(){
+		friendship_points +=1
+}
+
+for (i = 0; i < friendship_points; i++){
+	var img = document.createElement("<img src='octopus1.png'>");
+}
+	
